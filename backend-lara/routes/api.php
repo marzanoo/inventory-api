3d60c1ff124bd85660664349efa1bae24a4dd4ab
marzanoo <tanzanowayaw@gmail.com>
2025-02-06 09:58:46 +0700

    api route

--- backend-lara/routes/api.php
+++ backend-lara/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\MerekController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/merek', [MerekController::class, 'index']);
+Route::post('/merek', [MerekController::class, 'store']);
+Route::get('/merek/{id}', [MerekController::class, 'show']);
+Route::put('/merek/{id}', [MerekController::class, 'update']);
+Route::delete('/merek/{id}', [MerekController::class, 'destroy']);
